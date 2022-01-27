dcaf69c03f3c239041864ff3b6eaaa8a1aefd5e1
Rrezakk <52713544+Rrezakk@users.noreply.github.com>
2022-01-27 17:29:31 +0300

    for testing custom controller

---
+++ raii-sync/Features/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace raii_sync.Features
+{
+    [ApiController]
+    public class AuthController : Controller
+    {
+        [HttpGet("/auth")]
+        public string Index()
+        {
+            return "auth";
+        }
+    }
+}
